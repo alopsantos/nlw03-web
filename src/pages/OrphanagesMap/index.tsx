6ef64ad2f724ffe05df7cfedcca724c3e162dacd
alopsantos <anderson_lops@hotmail.com>
2020-10-15 21:06:19 -0300

    :sparkles: Pagina Orphaneg

--- src/pages/OrphanagesMap/index.tsx
+++ src/pages/OrphanagesMap/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import mapMarkerImg from '../../assets/img/map-maker.svg';
+import logo from '../../assets/img/Logo.svg';
+import './styles.css';
+import { FiPlus } from 'react-icons/fi';
 
 function OrphanagesMap(){
   return (
     <div id="page-map">
       <aside>
         <header>
-          <img src={mapMarkerImg} alt="Happy"/>
+          <img src={logo} alt="Happy"/>
           
           <h2>Escolha um orfanato no mapa</h2>
           <p>Muitas criancas estão esperando a sua visita :)</p>
@@ -18,6 +22,12 @@ function OrphanagesMap(){
           <strong>Parana</strong>
         </footer>
       </aside>
+      <div></div>
+
+      <Link to="/" className="create-orphanage">
+        <FiPlus size={32} color="#fff" />
+      </Link>
+
     </div>
   );
 }
