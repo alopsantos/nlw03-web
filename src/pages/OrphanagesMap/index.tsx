0de081f8b4d4f35e95215d5164d2d13c4e8172e7
Anderson Lopes dos Santos <lopscorp@iMac-de-Anderson.local>
2020-10-15 22:13:14 -0300

    feature :sparkles: - Maps

--- src/pages/OrphanagesMap/index.tsx
+++ src/pages/OrphanagesMap/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Map, TileLayer } from 'react-leaflet';
 
 import mapMarkerImg from '../../assets/img/map-maker.svg';
+import 'leaflet/dist/leaflet.css';
 import logo from '../../assets/img/Logo.svg';
 import './styles.css';
 import { FiPlus } from 'react-icons/fi';
@@ -22,7 +24,14 @@ function OrphanagesMap(){
           <strong>Parana</strong>
         </footer>
       </aside>
-      <div></div>
+      <Map 
+        center={[-25.4446519,-54.4011107]}
+        zoom={15}
+        style={{width:'100%', height:'100%'}}
+      >
+        {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+        <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
+      </Map>
 
       <Link to="/" className="create-orphanage">
         <FiPlus size={32} color="#fff" />
